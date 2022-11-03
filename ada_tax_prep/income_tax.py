bfd81a0f876594a6f816aa9fcbf64136a23f98dd
kimiahasani <kn.improve@gmail.com>
2022-11-02 18:34:13 -0700

    implements tax liability calculation

--- ada_tax_prep/income_tax.py
+++ ada_tax_prep/income_tax.py
@@ -63,3 +63,10 @@ def calculate_tax_by_bracket(income, brackets):
 def calculate_tax_2020(income):
     return calculate_tax_by_bracket(income, TAX_BRACKETS_2020)
 
+def calculate_tax_liability(income, deductions, standard_deduction, brackets):
+    deducted_income = calculate_deducted_income(income, deductions, standard_deduction)
+    return calculate_tax_by_bracket(deducted_income, brackets)
+
+def calculate_tax_liability_2020(income, deductions):
+    return calculate_tax_liability(income, deductions, STANDARD_DEDUCTION_2020, TAX_BRACKETS_2020)
+
